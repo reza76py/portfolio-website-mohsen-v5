235ec73f56c254108789a042dd0babf2ae5a10be
Reza Mollagholipour <reza76py@gmail.com>
2024-12-27 10:24:42 +1000

    only message

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [message, setMessage] = useState('')
+  useEffect(() => {
+    axios
+    .get('http://127.0.0.1:8000/api/message/')
+    .then((response) => {
+      setMessage(response.data.message)
+
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Message from the API</h1>
+      <p>{message}</p>
+    </div>
+  )
+}
+
+export default App
