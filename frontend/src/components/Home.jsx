d01168b2ebcd22454522e345fda3d11160ff6cb3
Reza Mollagholipour <reza76py@gmail.com>
2024-12-30 10:30:08 +1000

    responsive-v1, home_styles_v2, adding logo

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,71 +1,3 @@
-// import { useState, useEffect, useRef } from "react";
-// import axios from "axios";
-
-// const Home = () => {
-//   const [message, setMessage] = useState("");
-//   const [audioFiles, setAudioFiles] = useState([]);
-//   const audioRefs = useRef([]); // Use refs to manage audio elements
-
-//   useEffect(() => {
-//     axios.get("http://127.0.0.1:8000/api/message/").then((response) => {
-//       setMessage(response.data.message);
-//     });
-
-//     axios.get("http://127.0.0.1:8000/api/audio/").then((response) => {
-//       setAudioFiles(response.data);
-//       audioRefs.current = response.data.map(() => null); // Initialize refs
-//     });
-//   }, []);
-
-//   const handleAudioPlayPause = (index) => {
-//     const currentAudio = audioRefs.current[index];
-//     if (currentAudio) {
-//       if (currentAudio.paused) {
-//         currentAudio.play();
-//       } else {
-//         currentAudio.pause();
-//       }
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1>Home</h1>
-//       <p>{message}</p>
-//       <ul style={{ listStyleType: "none", padding: 0 }}>
-//         {audioFiles.map((audio, index) => (
-//           <li key={index} style={{ margin: "20px" }}>
-//             <div
-//               className="image-container"
-//               onClick={() => handleAudioPlayPause(index)} // Play/pause audio on image click
-//             >
-//               <img src={audio.image_url} alt={audio.title} />
-//               <div className="overlay">
-//                 <h3>{audio.title}</h3>
-//                 <p>{audio.description}</p>
-//               </div>
-//             </div>
-//             {/* Hidden audio element */}
-//             <audio ref={(el) => (audioRefs.current[index] = el)} src={audio.audio_url} />
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
-
-
-
-
-
-
-
-
-
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
@@ -98,7 +30,7 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Home</h1>
+      
       <p>{message}</p>
       <ul className="image-list">
         {audioFiles.map((audio, index) => (
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,19 +1,45 @@
-const navbar = () => {
-    return (
-        <div>
-            <ul className='navbar-nav'>
-                <li className='nav-item'>
-                    <a href='/'>Home</a>
-                </li>
-                <li className='nav-item'>
-                    <a href='/about'>About</a>
-                </li>
-                <li className='nav-item'>
-                    <a href='/contact'>Contact</a>
-                </li>
-            </ul>
-        </div>
-    )
+import React, { useState } from 'react';
+
+const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="navbar">
+      <img className='logo'
+      src='./public/logo.jpg'></img> {/* Replace with your actual logo */}
+      
+      {/* Hamburger Menu */}
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      
+      {/* Dropdown Menu */}
+      <div className={`dropdown-menu ${menuOpen ? 'active' : ''}`}>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+      
+      {/* Regular Navbar for larger screens */}
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="/about">About</a>
+        </li>
+        <li className="nav-item">
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
-export default navbar;
+export default NavBar;
